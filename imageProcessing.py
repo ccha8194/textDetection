a60536f2ee24f213e0de87ff2f2389b1e6e8a017
ccha8194 <85654927+ccha8194@users.noreply.github.com>
2023-07-30 23:11:38 -0400

    Update imageProcessing.py

--- imageProcessing.py
+++ imageProcessing.py
@@ -1,5 +1,7 @@
+import os
 import cv2
 import numpy as np
+import glob
 
 def resize_image(image, target_size=(640, 480)):
     return cv2.resize(image, target_size)
@@ -19,26 +21,42 @@ def preprocess_mask(mask, num_classes):
         one_hot_mask[:, :, class_idx] = (mask == class_idx).astype(np.uint8)
     return one_hot_mask
 
-def main():
-    input_image_path = 'input_image.jpg'
-    input_mask_path = 'input_mask.png'
-    image = cv2.imread(input_image_path)
-    mask = cv2.imread(input_mask_path, cv2.IMREAD_GRAYSCALE)
-
-    target_size = (640, 480)
-    image = resize_image(image, target_size)
-    mask = resize_image(mask, target_size)
-
-    grayscale_image = convert_to_grayscale(image)
-
-    enhanced_image = enhance_contrast(grayscale_image)
-
-    normalized_image = normalize_image(enhanced_image)
-
+def preprocess_folder(input_folder, output_folder):
     num_classes = 3
-    one_hot_mask = preprocess_mask(mask, num_classes)
-    
+    target_size = (640, 480)
+
+    image_files = glob.glob(os.path.join(input_folder, '*.jpg'))
+    mask_files = glob.glob(os.path.join(input_folder, '*.png'))
+
+    for image_path in image_files:
+        image = cv2.imread(image_path)
+        mask_path = os.path.join(input_folder, os.path.splitext(os.path.basename(image_path))[0] + '.png')
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+
+        image = resize_image(image, target_size)
+        mask = resize_image(mask, target_size)
+
+        grayscale_image = convert_to_grayscale(image)
+        enhanced_image = enhance_contrast(grayscale_image)
+        normalized_image = normalize_image(enhanced_image)
+
+        one_hot_mask = preprocess_mask(mask, num_classes)
+        output_image = np.concatenate((image, np.expand_dims(grayscale_image, axis=-1), np.expand_dims(enhanced_image, axis=-1)), axis=1)
+        output_image = np.concatenate((output_image, np.expand_dims(normalized_image, axis=-1)), axis=1)
+        for i in range(num_classes):
+            output_image = np.concatenate((output_image, np.expand_dims(one_hot_mask[:, :, i], axis=-1)), axis=1)
+
+        # Save the combined output image
+        output_path = os.path.join(output_folder, os.path.basename(image_path))
+        cv2.imwrite(output_path, output_image)
 
 if __name__ == "__main__":
-    main()
+    input_folder = 'input_folder'
+    output_folder = 'model_input' # <-- A little confusing, input for the model not this file :) 
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    preprocess_folder(input_folder, output_folder)
+
+
 
